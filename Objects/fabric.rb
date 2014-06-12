4313f686a421b1056a508baa99c4f9831787c808
Aleksuk <aleksuk.m@gmail.com>
2014-06-12 21:17:44 +0300

    Updated fabric

--- Objects/fabric.rb
+++ Objects/fabric.rb
@@ -4,25 +4,24 @@ class Fabric
         @@keys = args
     end
 
-    def new(*values)
+    def self.new(*keys, &block)
+        Class.new do
 
-        return Class.new do
+            @@keys = keys
 
-            def initialize(keys, *values)
-                @@param = []
+            def initialize(*values)
+                @param = []
 
-                keys.each do |key|
-                    if key.is_a?(Symbol)
-                        @@param << key
+                @@keys.each do |key|
+                    @param << key
 
-                        singleton_class.class_eval do
-                            attr_accessor key
-                        end
+                    singleton_class.class_eval do
+                        attr_accessor key
                     end
                 end
 
-                @@param.each_index do |index|
-                    self[index] = values[0][index]
+                values.each_index do |index|
+                    self[index] = values[index]
                 end
             end
 
@@ -38,7 +37,7 @@ class Fabric
 
             def get_method_name(key, isAssignment)
                 method_name = if key.is_a?(Fixnum)
-                    @@param[key].to_s
+                    @param[key].to_s
                 elsif key.is_a?(Symbol)
                     key.to_s
                 elsif key.is_a?(String)
@@ -48,14 +47,21 @@ class Fabric
                 method_name = (isAssignment) ? (method_name + '=').to_sym : method_name.to_sym
             end
 
-        end.new(@@keys, values)
+            public block.call if block
+        end
     end
 end
 
-temp_obj = Fabric.new(:a, :b)
-result = temp_obj.new(1, 2)
+new_type = Fabric.new(:a, :b) do
+    def hello_a
+        "hello #{a}"
+    end
+end
 
-p result.a
-p result[0]
-p result[:a]
-p result['a']
+new_obj = new_type.new(1, 2)
+p new_obj.a
+p new_obj[0]
+p new_obj[1]
+p new_obj[:a] = 5
+p new_obj['a']
+p new_obj.hello_a
